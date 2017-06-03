fe64c6af6ba26ab7b78cdc86f70738b71a746fca
ibnez32 <ibnezabed@gmail.com>
2017-06-03 03:00:03 -0400

    Sign out Page

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
         {'next_page': '/'},
         name = 'restaurant-sign-out'),
 
+    url(r'^restaurant/$', views.restaurant_home, name ='restaurant-home')
+
 ]
--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 # Redirect users to homepage with home function
 def home(request):
-    return render(request, 'home.html', {})
+    return redirect(restaurant_home)
+
+
+# First ask users for login information in sign in page, before going to the home page.
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html')
