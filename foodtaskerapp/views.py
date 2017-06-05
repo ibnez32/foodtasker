bc19923db43f52d1f54d54a42a4766827ff3c5e7
ibnez32 <ibnezabed@gmail.com>
2017-06-05 03:53:54 -0400

    Registration Sign Up Page

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -11,4 +11,7 @@ def home(request):
 # First ask users for login information in sign in page, before going to the home page.
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html')
+    return render(request, 'restaurant/home.html', {})
+
+def restaurant_sign_up(request):
+    return render (request, 'restaurant/sign_up.html', {})
